6c2ddb2d7f8be5d4bd05c183b8b8008becda1fca
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-05-24 23:23:50 +0530

    first commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,30 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import logo from '../public/logo.jpg'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <nav
-    className='border border-green-600 flex max-container padding-container
+    className='border-green-600 flexbetween max-container padding-container
     relative z-30 py-5'>
         <Link href='/'>
-          <p className='text-green-600'>Home</p>
+          <Image src={logo} alt='logo' width={50} height={50}/>
+
+          <ul className='hidden h-full gap-12 lg:flex'>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/about'>About</Link>
+            </li>
+            <li>
+              <Link href='/services'>Services</Link>
+            </li>
+            <li>
+              <Link href='/contact'>Contact</Link>
+            </li>
+          </ul>
         </Link>
     </nav>
   )
